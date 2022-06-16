10f41c3146e0b967d19424a94f0983c913a8803b
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-16 18:32:39 -0300

    feat: route for /videos/:id implemented

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -3,6 +3,8 @@ const VideoController = require("../controllers/video.controller.js");
 
 const router = express.Router();
 
-router.get("/videos", VideoController.listarVideos);
+router
+    .get("/videos/:id", VideoController.acessaVideoPorId)
+    .get("/videos", VideoController.listarVideos);
 
 module.exports = router;
