7ade9577cd352ca15fb2489e34467322570d1477
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-16 18:32:14 -0300

    feat: : "acessarVideoPorId" implemented

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,11 +2,27 @@ const Video = require("../models/video.model.js");
 
 class VideoController {
     static listarVideos = (req, res) => {
-        Video.find((err, Video) => {
+        Video.find((err, video) => {
             if (err) {
                 res.status(400).send({ message: err.message });
             } else {
-                res.status(200).json(Video);
+                res.status(200).json(video);
+            }
+        });
+    };
+
+    static acessaVideoPorId = (req, res) => {
+        const id = req.params.id;
+        console.log("params: " + req.params)
+        console.log("body: " + req.body)
+        Video.findById(id, (err, video) => {
+            if (err) {
+                res.status(400).send({
+                    message: `${err.message} - Video não encontado`,
+                });
+            } else {
+                console.log(`${video}`);
+                res.status(200).send(video);
             }
         });
     };
