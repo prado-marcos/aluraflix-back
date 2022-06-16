c9eb92aa73c483dde19fa5496cbf3ab34f29fa49
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-16 18:01:01 -0300

    feat: app implemented

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require("./routes/index.js");
+const db = require("./config/db.config.js");
+
+
+db.on("error", console.log.bind(console, "Erro de conexão"));
+db.once("open", ()=>{
+    console.log("Conexão com o banco feita com sucesso");
+})
+
+const app = express();
+
+routes(app);
+
+module.exports = app;
+
