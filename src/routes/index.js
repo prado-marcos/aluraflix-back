621030cb6c18f84f611139537823b708c2bc320b
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-23 12:06:32 -0300

    refactor: adcionar categoria ao app.use()

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 const video = require("./video.route.js");
+const categoria = require("./categoria.route.js");
 const express = require("express");
 
 const routes = (app) => {
     app.get("/", (req, res) => {
         res.status(200).send("Myflix");
     });
-    app.use(express.json(), video);
+    app.use(express.json(), video, categoria);
 };
 
 module.exports = routes;
