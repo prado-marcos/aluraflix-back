f7c1bf023849c4ac89d810f56af5d6f02678933d
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-23 11:58:01 -0300

    refactor: corrigir nome acessarVideoPorId; popular categoriaId em listarVideos

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,26 +2,25 @@ const Video = require("../models/video.model.js");
 
 class VideoController {
     static listarVideos = (req, res) => {
-        Video.find((err, video) => {
-            if (err) {
-                res.status(400).send({ message: err.message });
-            } else {
-                res.status(200).json(video);
-            }
-        });
+        Video.find()
+            .populate("categoriaId")
+            .exec((err, video) => {
+                if (err) {
+                    res.status(400).send({ message: err.message });
+                } else {
+                    res.status(200).json(video);
+                }
+            });
     };
 
-    static acessaVideoPorId = (req, res) => {
+    static acessarVideoPorId = (req, res) => {
         const id = req.params.id;
-        console.log("params: " + req.params);
-        console.log("body: " + req.body);
         Video.findById(id, (err, video) => {
             if (err) {
                 res.status(400).send({
                     message: `${err.message} - Video não encontado`,
                 });
             } else {
-                console.log(`${video}`);
                 res.status(200).send(video);
             }
         });
