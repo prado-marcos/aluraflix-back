444773c87db36995cf871c2a17311e729434c6d8
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-23 11:54:52 -0300

    style: corrigida a formatação

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ const express = require("express");
 const routes = require("./routes/index.js");
 const db = require("./config/db.config.js");
 
-
 db.on("error", console.log.bind(console, "Erro de conexão"));
-db.once("open", ()=>{
+db.once("open", () => {
     console.log("Conexão com o banco feita com sucesso");
-})
+});
 
 const app = express();
 
 routes(app);
 
 module.exports = app;
-
