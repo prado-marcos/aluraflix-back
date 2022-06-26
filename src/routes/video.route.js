1fbfed023e27cbf2ea5ab4c005c7370c84c3eb8c
Marcos Paulo Santos Prado <marcospaulo_prado@live.com>
2022-06-26 18:59:54 -0300

    "feat: adicionar rota /videos/search"

--- src/routes/video.route.js
+++ src/routes/video.route.js
@@ -4,8 +4,9 @@ const VideoController = require("../controllers/video.controller.js");
 const router = express.Router();
 
 router
-    .get("/videos", VideoController.listarVideos)
+    .get("/videos/search", VideoController.acessarVideosPorTitulo)
     .get("/videos/:id", VideoController.acessarVideoPorId)
+    .get("/videos", VideoController.listarVideos)
     .get("/categorias/:id/videos", VideoController.acessarVideosPorCategoria)
     .post("/videos", VideoController.cadastrarVideo)
     .put("/videos/:id", VideoController.atualizarVideo)
